1b8577c0b1eaee8f5c6f23a3343dff2f2438532c
Carlos Dimitri <carlosdimitriem@outlook.com>
2022-08-05 16:49:22 -0300

    Utilização da classe Util para distribuição de datasets para clientes

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import train_test_split
 from tensorflow import keras
 import pandas as pd
 from server import Server
+from utils import Utils
+
 
 initializer = tf.keras.initializers.RandomUniform(seed=2)
 initializer._random_generator._force_generator = True
@@ -46,8 +48,9 @@ model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='binary
 
 base = pd.read_csv('C:/Users/carlo/Desktop/Mestrado/Experimento FL/Etapa 1 - Escolha de modelo e data mining/IoTID20/IoTID20_preprocessada.csv')
 
-train_dataset, test_dataset = train_test_split(base, test_size=0.2, stratify=base['Label'])
+train_dataset, test_dataset = train_test_split(base, test_size=0.1, stratify=base['Label'])
 
+'''
 train_set_distribution = (train_dataset['Label'].value_counts()/train_dataset['Label'].count())*100
 test_set_distribution = (test_dataset['Label'].value_counts()/test_dataset['Label'].count())*100
 
@@ -55,23 +58,31 @@ print("Train dist")
 print(train_set_distribution)
 print("Test dist")
 print(test_set_distribution)
+'''
 
-client1  = Client(1, train_dataset)
-client2  = Client(2, train_dataset)
-client3  = Client(3, train_dataset)
+utl = Utils(base, 'Label', [[70,30], [30,70], [50,50]])
+
+
+
+groups_datasets_list = utl.get_samples_by_proportions()
+
+
+client1  = Client(1, groups_datasets_list[0])
+client2  = Client(2, groups_datasets_list[1])
+client3  = Client(3, groups_datasets_list[2])
 clients_list = [client1, client2, client3]
 clients_dataset_sizes = [client1.get_dataset_size(), client2.get_dataset_size(), client3.get_dataset_size()]
 
             #clients, dataset_sizes, model, clients_local_epochs, batch_size, global_epochs
-server = Server(clients_list, clients_dataset_sizes, model, 10, None, 2)
+server = Server(clients_list, clients_dataset_sizes, model, 50, None, 10)
 
 global_model = server.train_federated()
 
-'''for client in clients_list:
+for client in clients_list:
     print("Client: " + str(client.get_client_id()))
-    print(client.get_metrics(test_dataset))'''
+    print(client.get_metrics(test_dataset))
     
     
-print(server.get_metrics(train_dataset))
+#print(server.get_metrics(train_dataset))
     
-print(server.get_metrics(test_dataset))
+#print(server.get_metrics(test_dataset))
