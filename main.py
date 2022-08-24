f77a61ea1264e2f037a0293dfb1e274fbdbbd137
Carlos Dimitri <carlosdimitriem@outlook.com>
2022-08-24 13:11:55 -0300

    Novo main para os experimentos

--- main.py
+++ main.py
@@ -5,32 +5,16 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 
 from client import Client
-from sklearn.model_selection import train_test_split
 from tensorflow import keras
 import pandas as pd
 from server import Server
 from utils import Utils
-import copy
-
+from sklearn.model_selection import StratifiedKFold
 
 initializer = tf.keras.initializers.RandomUniform(seed=2)
 initializer._random_generator._force_generator = True
 bias = tf.keras.initializers.Zeros()
-
-'''
-model = keras.models.Sequential([
-    keras.layers.Dense(2, 
-                       input_dim=12, 
-                       activation = 'relu',
-                       kernel_initializer = initializer,
-                       bias_initializer = bias),
-    keras.layers.Dense(1, 
-                       activation = 'sigmoid',
-                       kernel_initializer = initializer,
-                       bias_initializer = bias),
-])
-
-'''
+max_clients = 5
 
 model = keras.models.Sequential([
     keras.layers.Dense(30, 
@@ -54,34 +38,54 @@ model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='binary
 
 base = pd.read_csv('C:/Users/carlo/Desktop/Mestrado/Experimento FL/Etapa 1 - Escolha de modelo e data mining/IoTID20/IoTID20_preprocessada.csv')
 
-                 #83 17
-train_dataset, test_dataset = train_test_split(base, test_size=0.1, stratify=base['Label'])
+skf = StratifiedKFold(n_splits = 10)
 
-utl = Utils(train_dataset, 'Label', [[70,30], [30, 70], [50, 50], [100, 0], [0, 100]])
+base = base.iloc[:, :]
+y = base.iloc[:, len(base.columns) - 1]
 
-groups_datasets_list = utl.get_samples_by_proportions()
-#groups_datasets_list = utl.get_stratified_sample()
-
-client1  = Client(1, groups_datasets_list[0])
-client2  = Client(2, groups_datasets_list[1])
-client3  = Client(3, groups_datasets_list[2])
-client4  = Client(4, groups_datasets_list[3])
-client5  = Client(5, groups_datasets_list[4])
-clients_list = [client1, client2, client3, client4, client5]
-clients_dataset_sizes = [client1.get_dataset_size(), client2.get_dataset_size(), client3.get_dataset_size(), client4.get_dataset_size(), client5.get_dataset_size()]
-
-            #clients, dataset_sizes, model, clients_local_epochs, batch_size, global_epochs
-#server = Server(clients_list, clients_dataset_sizes, model, 100, None, 100)
-server = Server(clients_list, clients_dataset_sizes, model, 100, None, 100)
-
-server.train_federated()
-
-#FAZER O CROSS VALIDATION
-'''
-for client in clients_list:
-    validation_set = copy.deepcopy(test_dataset)
-    print("Client: " + str(client.get_client_id()))
-    print(client.get_metrics(validation_set))
-'''
-print("Modelo do servidor")
-print(server.get_metrics(test_dataset))
+skf_iteration = 1
+with open('results.txt', 'w') as f:
+    for train_index, test_index in skf.split(base, y):
+        train_dataset = base.iloc[train_index]
+        test_dataset = base.iloc[test_index]
+        # O tamanho de [[70,30], [30, 70], [50, 50], [100, 0], [0, 100]] deve ser o mesmo da qtd de clientes
+        utl = Utils(train_dataset, 'Label', [[70,30], [30, 70], [50, 50], [100, 0], [0, 100]])
+        groups_datasets_list = utl.get_stratified_sample()
+        print(len(groups_datasets_list))
+        #Cria clientes
+        clients_list = []
+        for i in range(0, max_clients):
+            client = Client(i + 1, groups_datasets_list[i])
+            clients_list.append(client)
+            
+        #Pega o tamanho do dataset dos clientes
+        clients_dataset_sizes = []
+        for client in clients_list:
+            clients_dataset_sizes.append(client.get_dataset_size())
+        
+        server = Server(clients_list, clients_dataset_sizes, model, 100, None, 100)
+    
+        server.train_federated()
+                
+        f.write("Iteração do Cross Validation Estratificado: " + str(skf_iteration))
+        f.write('\n')
+            
+        print("Iteração do Cross Validation Estratificado: " + str(skf_iteration))
+        for client in clients_list:
+            #f.write("Client: " + str(client.get_client_id()))
+            #f.write('\n')
+            #f.write(client.get_metrics(validation_set))
+            #f.write('\n')
+            print("Client: " + str(client.get_client_id()))
+            print(client.get_metrics(test_dataset))
+    
+        f.write("Modelo do servidor")
+        f.write('\n')
+        f.write(server.get_metrics(test_dataset))
+        f.write('\n')
+        print("Modelo do servidor")
+        print(server.get_metrics(test_dataset))
+        
+        
+        skf_iteration = skf_iteration + 1
+    
